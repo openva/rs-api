eef0fc08423de37eef594b6022ed4fa3965b219e
Waldo Jaquith <waldo@jaquith.org>
2018-01-26 23:08:07 -0500

    Include portfolio-owner data

--- htdocs/1.1/photosynthesis.php
+++ htdocs/1.1/photosynthesis.php
@@ -32,6 +32,52 @@ if (isset($_REQUEST['callback']) && !empty($_REQUEST['callback']))
 	$callback = $_REQUEST['callback'];
 }
 
+# Get this portfolio's basic data.
+$sql = 'SELECT dashboard_portfolios.id, dashboard_portfolios.hash, dashboard_portfolios.name,
+		dashboard_portfolios.notes, users.name AS user_name, dashboard_user_data.organization,
+		users.url
+		FROM dashboard_portfolios
+		LEFT JOIN users
+			ON dashboard_portfolios.user_id = users.id
+		LEFT JOIN dashboard_user_data
+			ON users.id = dashboard_user_data.user_id
+		WHERE dashboard_portfolios.public = "y" AND dashboard_portfolios.hash="' . $hash . '"';
+$result = mysql_query($sql);
+
+# If this portfolio doesn't exist or isn't visible.
+if (mysql_num_rows($result) == 0)
+{
+	header("Status: 404 Not Found");
+	$message = array('error' =>
+		array('message' => 'No Portfolio Found',
+			'details' => 'Portfolio ' . $hash . ' does not exist.'));
+	echo json_encode($message);
+	exit;
+}
+
+
+$portfolio = mysql_fetch_array($result, MYSQL_ASSOC);
+$portfolio = array_map('stripslashes', $portfolio);
+	
+# Make the user closer to anonymous.
+$tmp = explode(' ', $portfolio['user_name']);
+if (count($tmp) > 1)
+{
+	$portfolio['user_name'] = $tmp[0].' '.$tmp[1]{0}.'.';
+}
+else
+{
+	$portfolio['user_name'] = $tmp[0];
+}
+if (isset($portfolio['organization']))
+{
+	unset($portfolio['user_name']);
+}
+unset($portfolio['id']);
+unset($portfolio['hash']);
+unset($portfolio['name']);
+unset($portfolio['notes']);
+
 # Select the bill data from the database.
 $sql = 'SELECT bills.number, sessions.year, dashboard_bills.notes
 		FROM dashboard_portfolios
@@ -54,7 +100,7 @@ if (mysql_num_rows($result) == 0)
 	header("Status: 404 Not Found");
 	$message = array('error' =>
 		array('message' => 'No Bills Found',
-			'details' => 'No bills were found in portfolio '.$hash.'.'));
+			'details' => 'No bills were found in portfolio ' . $hash . '.'));
 	echo json_encode($message);
 	exit;
 }
@@ -62,11 +108,12 @@ if (mysql_num_rows($result) == 0)
 # Build up a listing of all bills.
 # The MYSQL_ASSOC variable indicates that we want just the associated array, not both associated
 # and indexed arrays.
+$portfolio['bills'] = array();
 while ($bill = mysql_fetch_array($result, MYSQL_ASSOC))
 {
 	$bill['url'] = 'http://www.richmondsunlight.com/bill/'.$bill['year'].'/'.$bill['number'].'/';
 	$bill['number'] = strtoupper($bill['number']);
-	$bills[] = array_map('stripslashes', $bill);
+	$portfolio['bills'][] = array_map('stripslashes', $bill);
 }
 
 # Send an HTTP header defining the content as JSON.
@@ -81,7 +128,7 @@ if (isset($callback))
 {
 	echo $callback.' (';
 }
-echo json_encode($bills);
+echo json_encode($portfolio);
 if (isset($callback))
 {
 	echo ');';
