e8732343cf9c2cd8c24096f146825c8c4410ec62
Waldo Jaquith <waldo@jaquith.org>
2019-01-20 15:09:34 -0500

    Fix the includes path

--- docker-build.sh
+++ docker-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git clone https://github.com/openva/richmondsunlight.com.git
-mv richmondsunlight.com/htdocs/includes ./includes/
+mv richmondsunlight.com/htdocs/includes htdocs/includes/
 rm -Rf richmondsunlight.com/
 
 docker-compose build && docker-compose up
