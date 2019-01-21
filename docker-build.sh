c38b6dd605c1a22b962ab7f6040eeb0f227b452f
Waldo Jaquith <waldo@jaquith.org>
2019-01-20 20:29:42 -0500

    Expand the Docker script

--- docker-build.sh
+++ docker-build.sh
@@ -4,4 +4,7 @@ git clone https://github.com/openva/richmondsunlight.com.git
 mv richmondsunlight.com/htdocs/includes htdocs/includes/
 rm -Rf richmondsunlight.com/
 
+// set up settings.inc.php
+// at LEAST rename the damn thing
+
 docker-compose build && docker-compose up
