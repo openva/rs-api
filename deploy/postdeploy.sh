36013c3496fec122823b1a2ca96585095fc7cfe2
Waldo Jaquith <waldo@jaquith.org>
2019-03-18 22:10:46 -0400

    Set up a proper deploy process

--- deploy/postdeploy.sh
+++ deploy/postdeploy.sh
@@ -3,3 +3,18 @@
 # Set permissions properly, since appspec.yml gets this wrong.
 chown -R ubuntu:ubuntu /var/www/api.richmondsunlight.com/
 chmod -R g+w /var/www/api.richmondsunlight.com/
+
+# Set up Apache, if need be.
+SITE_SET_UP="$(sudo apache2ctl -S 2>&1 |grep -c api.richmondsunlight.com)"
+if [ "$SITE_SET_UP" -eq "0" ]; then
+
+    # Set up Apache
+    sudo cp deploy/virtualhost.txt /etc/apache2/sites-available/api.richmondsunlight.com.conf
+    sudo a2ensite api.richmondsunlight.com
+    sudo a2enmod headers expires rewrite http2
+    sudo systemctl reload apache2
+
+    # Install a certificate
+    sudo certbot --apache -d api.richmondsunlight.com --non-interactive --agree-tos --email jaquith@gmail.com --redirect
+
+fi
--- deploy/predeploy.sh
+++ deploy/predeploy.sh
@@ -1,17 +1,49 @@
 #!/bin/bash
 
-# Change the timezone to Eastern
-#sudo cp /usr/share/zoneinfo/US/Eastern /etc/localtime
+# If the site doesn't already exist, then this is a fresh server.
+SITE_SET_UP="$(sudo apache2ctl -S |grep -c api.richmondsunlight.com)"
+if [ "$SITE_SET_UP" -eq "0" ]; then
 
-# Remove all PHP packages (they may well be PHP 7)
-#sudo apt-get -y purge `dpkg -l | grep php| awk '{print $2}' |tr "\n" " "`
+    # Set the timezone to Eastern
+    sudo cp /usr/share/zoneinfo/US/Eastern /etc/localtime
 
-# Use repo for PHP 5.6.
-#sudo add-apt-repository -y ppa:ondrej/php
+    # Remove all PHP packages (they may well be PHP 7)
+    dpkg -s php5.7
+    if [ $? -eq 1 ]; then
+        sudo apt-get -y purge $(dpkg -l | grep php| awk '{print $2}' |tr "\n" " ")
 
-# Update the OS
-#sudo apt-get update
-#sudo apt-get -y upgrade
+        # Add the PHP 5 repo
+        sudo add-apt-repository -y ppa:ondrej/php
+    fi
 
-# Install necessary packages.
-#sudo apt-get install -y php5.6-cli php5.6-mysql php5.6-memcached awscli
+    # Add the Certbot repo
+    dpkg -s certbot
+    if [ $? -eq 1 ]; then
+        sudo add-apt-repository -y ppa:certbot/certbot
+    fi
+
+    # Install all packages.
+    sudo apt-get update
+    sudo DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y apache2 curl git gzip unzip openssl php5.6 php5.6-mysql mysql-client php5.6-curl php5.6-mbstring php5.6-apc php5.6-mbstring php5.6-xml python python-pip s3cmd wget awscli certbot python-certbot-apache
+
+    # Install mod_pagespeed
+    dpkg -s mod-pagespeed-beta
+    if [ $? -eq 1 ]; then
+        wget https://dl-ssl.google.com/dl/linux/direct/mod-pagespeed-beta_current_amd64.deb
+        sudo dpkg -i mod-pagespeed-*.deb
+        sudo apt-get -f install
+        rm mod-pagespeed-*.deb
+    fi
+
+    # Install Certbot
+    dpkg -s certbot
+    if [ $? -eq 1 ]; then
+        sudo apt-get install -y ruby
+        wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+        chmod +x ./install
+        sudo ./install auto
+        rm install
+    fi
+    
+fi
