f7010c4cfebdaa860fb4b77bdac5b44e9d698a77
Waldo Jaquith <waldo@jaquith.org>
2019-03-23 22:14:17 -0400

    Add a test to validate a legislator query response
    Closes #13.

---
+++ deploy/tests/legislator.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+# Hit the API for a legislator, save the JSON to a temporary file.
+curl -s -o /tmp/legislator.json http://localhost:5001/1.1/legislator/rbbell.json
+
+# Compare the API response to the known-good response.
+cmp -s /tmp/legislator.json legislator.json > /dev/null
+
+# If the response is different, show exactly how and error out.
+if [ $? -eq 1 ]; then
+    diff <(jq -S . temp.json) <(jq -S . legislator.json)
+    rm /tmp/legislator.json
+    exit 1
+fi
+
+# Delete our temporary file.
+rm /tmp/legislator.json
