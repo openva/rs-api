7b184e1286903b529578a9f0403f6fc457d0cd49
Waldo Jaquith <waldo@jaquith.org>
2025-10-26 18:58:41 -0400

    Be specific about the location of virtualhost.txt

--- deploy/postdeploy.sh
+++ deploy/postdeploy.sh
@@ -4,12 +4,14 @@
 chown -R ubuntu:ubuntu /var/www/api.richmondsunlight.com/
 chmod -R g+w /var/www/api.richmondsunlight.com/
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # Set up Apache, if need be.
 SITE_SET_UP="$(sudo apache2ctl -S 2>&1 |grep -c api.richmondsunlight.com)"
 if [ "$SITE_SET_UP" -eq "0" ]; then
 
     # Set up Apache
-    sudo cp virtualhost.txt /etc/apache2/sites-available/api.richmondsunlight.com.conf
+    sudo cp "${SCRIPT_DIR}/virtualhost.txt" /etc/apache2/sites-available/api.richmondsunlight.com.conf
     sudo a2ensite api.richmondsunlight.com
     sudo a2enmod headers expires rewrite http2
     sudo systemctl reload apache2
