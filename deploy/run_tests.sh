471057e74c4fe8947c388642d4b70b005149907f
Waldo Jaquith <waldo@jaquith.org>
2026-01-06 19:20:07 -0500

    Ensure that Docker can be found

--- deploy/run_tests.sh
+++ deploy/run_tests.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Ensure common binary locations are in PATH (needed for non-interactive shells)
+# Includes ~/.docker/bin for newer Docker Desktop installations on macOS
+export PATH="$HOME/.docker/bin:/usr/local/bin:/opt/homebrew/bin:/usr/bin:/bin:$PATH"
+
 if [ "$(docker container ps |grep -c rs_api)" -eq "0" ]; then
     echo "The API is not running in Docker, so cannot run tests"
     exit 1
